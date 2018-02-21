0280aa8ea23b8240439ca6a6a49d3ca0df29ecbc
Jose Alejandro Silva Ariza <ja.silva11@uniandes.edu.co>
2018-02-21 17:34:50 +0100

    [ELASTICSEARCH] Suggestion d'acteurs

--- oracle-of-bacon-backend/src/main/java/com/serli/oracle/of/bacon/api/APIEndPoint.java
+++ oracle-of-bacon-backend/src/main/java/com/serli/oracle/of/bacon/api/APIEndPoint.java
@@ -38,11 +38,7 @@ public class APIEndPoint {
 
     @Get("suggest?q=:searchQuery")
     public List<String> getActorSuggestion(String searchQuery) throws IOException {
-    	return Arrays.asList("Niro, Chel",
-                "Senanayake, Niro",
-                "Niro, Juan Carlos",
-                "de la Rua, Niro",
-                "Niro, Simão");
+    	return elasticSearchRepository.getActorsSuggests(searchQuery);
     }
 
     @Get("last-searches")
