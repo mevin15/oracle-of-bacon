edc7e765867196148ac3e0d7f72c930bb7d9627e
Jose Alejandro Silva Ariza <ja.silva11@uniandes.edu.co>
2018-02-21 17:30:47 +0100

    [ELASTICSEARCH] Importer les donnees

--- oracle-of-bacon-backend/src/main/java/com/serli/oracle/of/bacon/loader/elasticsearch/CompletionLoader.java
+++ oracle-of-bacon-backend/src/main/java/com/serli/oracle/of/bacon/loader/elasticsearch/CompletionLoader.java
@@ -2,47 +2,93 @@ package com.serli.oracle.of.bacon.loader.elasticsearch;
 
 import com.serli.oracle.of.bacon.repository.ElasticSearchRepository;
 
+import org.elasticsearch.action.admin.indices.mapping.put.PutMappingRequest;
 import org.elasticsearch.action.index.IndexRequest;
 import org.elasticsearch.client.RestHighLevelClient;
+import org.elasticsearch.common.xcontent.XContentBuilder;
+import org.elasticsearch.common.xcontent.XContentFactory;
+import org.elasticsearch.common.xcontent.XContentType;
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 import java.util.concurrent.atomic.AtomicInteger;
 
 public class CompletionLoader {
-    private static AtomicInteger count = new AtomicInteger(0);
+	private static AtomicInteger count = new AtomicInteger(0);
 
-    public static void main(String[] args) throws IOException, InterruptedException {
-        RestHighLevelClient client = ElasticSearchRepository.createClient();
+	public static void main(String[] args) throws IOException, InterruptedException {
+		RestHighLevelClient client = ElasticSearchRepository.createClient();
 
-        if (args.length != 1) {
-            System.err.println("Expecting 1 arguments, actual : " + args.length);
-            System.err.println("Usage : completion-loader <actors file path>");
-            System.exit(-1);
-        }
+		if (args.length != 1) {
+			System.err.println("Expecting 1 arguments, actual : " + args.length);
+			System.err.println("Usage : completion-loader <actors file path>");
+			System.exit(-1);
+		}
 
-        String inputFilePath = args[0];
-        try (BufferedReader bufferedReader = Files.newBufferedReader(Paths.get(inputFilePath))) {
-            bufferedReader
-                    .lines()
-                    .forEach(line -> {
-                        //TODO ElasticSearch insert
-                        IndexRequest  indexRequest = new IndexRequest("actors", "actor").source("name", line);                        
-                    	try {
-							client.index(indexRequest);
-							count.addAndGet(1);
-						} catch (IOException e) {
-							// TODO Auto-generated catch block
-							e.printStackTrace();
-						}   
-                        System.out.println(line);
-                    });
-        }
+		String inputFilePath = args[0];
+		try (BufferedReader bufferedReader = Files.newBufferedReader(Paths.get(inputFilePath))) {
+			bufferedReader
+			.lines()
+			.forEach(line -> {
+				//TODO ElasticSearch insert                       
+				try {
+					Map<String, Object> map = new HashMap<>();
 
-        System.out.println("Inserted total of " + count.get() + " actors");
+					line = line.substring(1, line.length() - 1);
+					map.put("name", line);  
 
-        client.close();
-    }
+					List<String> suggestions = new ArrayList<String>();
+					String[] split = line.split(", ");
+					for(int i =0; i< split.length; i++){
+						suggestions.add(split[i]);
+						String[] split2 = split[i].split(" ");
+						if(split2.length > 1)
+						{
+							for(int j =0; j< split2.length; j++){
+								suggestions.add(split2[j]);
+							}
+						}
+					}
+					map.put("suggestion", suggestions);
+
+					PutMappingRequest request = new PutMappingRequest("actors"); 
+					request.type("actor"); 
+
+					XContentBuilder builder = XContentFactory.jsonBuilder();
+					builder.startObject();
+					{
+						builder.startObject("properties");
+						{
+							builder.field("name", "text");
+							builder.field("suggestion", "completion");
+						}
+						builder.endObject();
+					}
+					builder.endObject();
+					request.source(builder);
+					
+					//FAUT AJOUTER MAPPING A L'INDEX
+
+					IndexRequest  indexRequest = new IndexRequest("actors", "actor").source(map); 
+					client.index(indexRequest);
+					count.addAndGet(1);
+
+				} catch (IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}              	
+				System.out.println(line);
+			});
+		}
+
+		System.out.println("Inserted total of " + count.get() + " actors");
+
+		client.close();
+	}
 }
