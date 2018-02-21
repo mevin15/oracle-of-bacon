66a9b2e0e2708e20d0fb6ec58e6814bbe32f002d
Jose Alejandro Silva Ariza <ja.silva11@uniandes.edu.co>
2018-02-21 17:31:52 +0100

    [ELASTICSEARCH] Suggestion d'acteurs

--- oracle-of-bacon-backend/src/main/java/com/serli/oracle/of/bacon/repository/ElasticSearchRepository.java
+++ oracle-of-bacon-backend/src/main/java/com/serli/oracle/of/bacon/repository/ElasticSearchRepository.java
@@ -1,10 +1,20 @@
 package com.serli.oracle.of.bacon.repository;
 
 import org.apache.http.HttpHost;
+import org.elasticsearch.action.search.SearchRequest;
+import org.elasticsearch.action.search.SearchResponse;
 import org.elasticsearch.client.RestClient;
 import org.elasticsearch.client.RestHighLevelClient;
+import org.elasticsearch.common.unit.Fuzziness;
+import org.elasticsearch.search.builder.SearchSourceBuilder;
+import org.elasticsearch.search.suggest.Suggest;
+import org.elasticsearch.search.suggest.SuggestBuilder;
+import org.elasticsearch.search.suggest.SuggestBuilders;
+import org.elasticsearch.search.suggest.SuggestionBuilder;
+import org.elasticsearch.search.suggest.completion.CompletionSuggestion;
 
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 
 public class ElasticSearchRepository {
@@ -26,6 +36,29 @@ public class ElasticSearchRepository {
 
     public List<String> getActorsSuggests(String searchQuery) throws IOException {
         // TODO implement suggest
-        return null;
+    	List<String> suggestions = new ArrayList<String>();     
+
+        SearchSourceBuilder searchSourceBuilder = new SearchSourceBuilder();
+        
+        SuggestionBuilder<?> completionSuggestionBuilder =
+            SuggestBuilders.completionSuggestion("suggestion").prefix(searchQuery, Fuzziness.AUTO).size(5).skipDuplicates(true); 
+        
+        SuggestBuilder suggestBuilder = new SuggestBuilder();
+        suggestBuilder.addSuggestion("suggest_actor", completionSuggestionBuilder); 
+        searchSourceBuilder.suggest(suggestBuilder);
+        
+        SearchRequest searchRequest = new SearchRequest();        
+        searchRequest.source(searchSourceBuilder);
+        SearchResponse searchResponse = client.search(searchRequest); 
+        
+        Suggest suggest = searchResponse.getSuggest(); 
+        CompletionSuggestion completionSuggestion = suggest.getSuggestion("suggest_actor"); 
+        
+        for (CompletionSuggestion.Entry entry : completionSuggestion.getEntries()) { 
+            for (CompletionSuggestion.Entry.Option option : entry) { 
+                suggestions.add(option.getHit().getSourceAsMap().get("name").toString());
+            }
+        }
+        return suggestions;
     }
 }
