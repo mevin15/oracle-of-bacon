bbfe63531cfbd555d12aa28d774682eb101a9065
mevin15 <morgane.evin3@gmail.com>
2018-02-21 10:27:37 +0100

    [NEO4J] Implémentation de chemin oracle-de-bacon avec Neo4j

--- oracle-of-bacon-backend/src/main/java/com/serli/oracle/of/bacon/api/APIEndPoint.java
+++ oracle-of-bacon-backend/src/main/java/com/serli/oracle/of/bacon/api/APIEndPoint.java
@@ -1,5 +1,6 @@
 package com.serli.oracle.of.bacon.api;
 
+import com.google.gson.Gson;
 import com.serli.oracle.of.bacon.repository.ElasticSearchRepository;
 import com.serli.oracle.of.bacon.repository.MongoDbRepository;
 import com.serli.oracle.of.bacon.repository.Neo4JRepository;
@@ -25,46 +26,10 @@ public class APIEndPoint {
 
     @Get("bacon-to?actor=:actorName")
     public String getConnectionsToKevinBacon(String actorName) {
-
-        return "[\n" +
-                "{\n" +
-                "\"data\": {\n" +
-                "\"id\": 85449,\n" +
-                "\"type\": \"Actor\",\n" +
-                "\"value\": \"Bacon, Kevin (I)\"\n" +
-                "}\n" +
-                "},\n" +
-                "{\n" +
-                "\"data\": {\n" +
-                "\"id\": 2278636,\n" +
-                "\"type\": \"Movie\",\n" +
-                "\"value\": \"Mystic River (2003)\"\n" +
-                "}\n" +
-                "},\n" +
-                "{\n" +
-                "\"data\": {\n" +
-                "\"id\": 1394181,\n" +
-                "\"type\": \"Actor\",\n" +
-                "\"value\": \"Robbins, Tim (I)\"\n" +
-                "}\n" +
-                "},\n" +
-                "{\n" +
-                "\"data\": {\n" +
-                "\"id\": 579848,\n" +
-                "\"source\": 85449,\n" +
-                "\"target\": 2278636,\n" +
-                "\"value\": \"PLAYED_IN\"\n" +
-                "}\n" +
-                "},\n" +
-                "{\n" +
-                "\"data\": {\n" +
-                "\"id\": 9985692,\n" +
-                "\"source\": 1394181,\n" +
-                "\"target\": 2278636,\n" +
-                "\"value\": \"PLAYED_IN\"\n" +
-                "}\n" +
-                "}\n" +
-                "]";
+    	List<?> connections = neo4JRepository.getConnectionsToKevinBacon(actorName);
+    	// Convert array in string 
+    	String json = new Gson().toJson(connections);
+    	return json; 	
     }
 
     @Get("suggest?q=:searchQuery")
--- oracle-of-bacon-backend/src/main/java/com/serli/oracle/of/bacon/repository/Neo4JRepository.java
+++ oracle-of-bacon-backend/src/main/java/com/serli/oracle/of/bacon/repository/Neo4JRepository.java
@@ -5,21 +5,67 @@ import org.neo4j.driver.v1.AuthTokens;
 import org.neo4j.driver.v1.Driver;
 import org.neo4j.driver.v1.GraphDatabase;
 import org.neo4j.driver.v1.Session;
+import org.neo4j.driver.v1.StatementResult;
+import org.neo4j.driver.v1.Transaction;
+import org.neo4j.driver.v1.types.Path;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
 public class Neo4JRepository {
     private final Driver driver;
 
     public Neo4JRepository() {
-        this.driver = GraphDatabase.driver("bolt://localhost:7687", AuthTokens.basic("neo4j", "password"));
+        this.driver = GraphDatabase.driver("bolt://localhost:7687", AuthTokens.basic("neo4j", "neo4j"));
     }
 
     public List<?> getConnectionsToKevinBacon(String actorName) {
         Session session = driver.session();
-
-        // TODO implement Oracle of Bacon
-        return null;
+        ArrayList<HashMap<String, GraphItem>> results = new ArrayList<HashMap<String, GraphItem>>();
+        
+        try (Transaction tx = session.beginTransaction() )
+        {
+        	StatementResult requestResult = tx.run( "MATCH p=shortestPath( (bacon:Actors {name:\"Bacon, Kevin (I)\"})-[*1..6]-(meg:Actors {name: \""+ actorName +"\"}) ) RETURN bacon, p, meg");
+        	
+        	if (requestResult.hasNext()) {
+        		Path p  = (Path) requestResult.next().asMap().get("p");
+        		
+        		p.nodes().forEach(node -> {
+        			GraphNode gn;
+        			String name; 
+        			
+        			// For Actors node
+        			if (node.containsKey("name")) {
+        				name = node.get("name").asString();
+        				gn = new GraphNode(node.id(), name, "Actor");
+        				HashMap<String, GraphItem> hm = new HashMap<String, GraphItem>();
+            			hm.put("data", gn);
+            			results.add(hm);
+        			}
+        			
+        			// For Movies node 
+        			if (node.containsKey("title")){
+        				name = node.get("title").asString();
+        				gn = new GraphNode(node.id(), name, "Actor");
+        				HashMap<String, GraphItem> hm = new HashMap<String, GraphItem>();
+            			hm.put("data", gn);
+            			results.add(hm);
+        			}
+        		});
+        		
+        		p.relationships().forEach(relation -> {
+        			GraphEdge ge; 
+        			
+        			ge = new GraphEdge(relation.id(), relation.startNodeId(), relation.endNodeId(),"PLAYED_IN");
+        			HashMap<String, GraphItem> hm = new HashMap<String, GraphItem>();
+        			hm.put("data", ge);
+        			results.add(hm);
+        		});
+        		tx.success();	
+        	}
+            return results;
+        }
     }
 
     public static abstract class GraphItem {
